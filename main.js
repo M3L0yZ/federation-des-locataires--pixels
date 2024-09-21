17eba4a83648f1ebea79d274ed8cda0fe1451f36
audreydandurand <112189528+audreydandurand@users.noreply.github.com>
2024-09-21 10:32:01 -0400

    amélioration fonction

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 function myFunction() {
   var x = document.getElementsByClassName("nav__list")[0];
-  if (x.style.display === "block") {
+  if (window.matchMedia("(max-width:1400px)").matches ) {
+    if (x.style.display === "block") {
     x.style.display = "none";
   } else {
     x.style.display = "block";
   }
+  }
+  
 }
