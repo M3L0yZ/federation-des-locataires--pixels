b5a564d50466061851dac24c0687a7b7dee44b50
audreydandurand <112189528+audreydandurand@users.noreply.github.com>
2024-09-22 09:42:17 -0400

    ajustement problèmes mobile/desktop

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 function myFunction() {
-  var x = document.getElementsByClassName("nav__list")[0];
-  if (window.matchMedia("(max-width:1400px)").matches ) {
-    if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-  }
-  
+    var list = document.getElementsByClassName("nav__list")[0];
+    if (window.matchMedia("(max-width:1400px)").matches) {
+        list.classList.toggle('list-active');
+    } else {
+        list.classList.remove('list-active');
+    }
 }
+
+function TailleFenetre() {
+    var list = document.getElementsByClassName("nav__list")[0];
+    if (window.innerWidth >= 1400) {
+        list.classList.remove('list-active');
+    } else {
+        list.classList.remove('list-active');
+    }
+}
+
+window.addEventListener('resize', TailleFenetre);
