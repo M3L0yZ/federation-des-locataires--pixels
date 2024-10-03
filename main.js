26ea0a1a088683349e457d4c3da8a55574b13f71
audreydandurand <112189528+audreydandurand@users.noreply.github.com>
2024-10-03 11:35:54 -0400

    ajustement visuel voulu tablette

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 
 var swiper = new Swiper(".swiper-container", {
     loop: true,
+    slidesPerView: 2,
+      centeredSlides: true,
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
