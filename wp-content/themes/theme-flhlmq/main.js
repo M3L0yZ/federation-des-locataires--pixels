975001f4a48d14a4c04d8a745634a81aa6a4033a
audreydandurand <112189528+audreydandurand@users.noreply.github.com>
2024-11-07 10:02:47 -0500

    gsap bogue merge
    Tout a été effacé lors du merge donc remise du code fait hier.

--- wp-content/themes/theme-flhlmq/main.js
+++ wp-content/themes/theme-flhlmq/main.js
@@ -1,88 +1,139 @@
 var swiper = new Swiper(".swiper-container", {
-    loop: true,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
 
 var swiper2 = new Swiper(".swiper-container2", {
-    loop: true,
-    slidesPerView: 2,
-    centeredSlides: true,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
+  loop: true,
+  slidesPerView: 2,
+  centeredSlides: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
 
 var swiper2 = new Swiper(".mySwiperHub", {
-    loop: false,
-    centeredSlides: true,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-        renderBullet: function (index, className) {
-            return '<span class="' + className + '">' + (index + 1) + "</span>";
-          },
-      },
+  loop: false,
+  centeredSlides: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  },
 });
+// ---------------------- PAGE NOUVELLE HUB -> EN CONTINU ------------------------
 
-let currentSlide = 0;
+let slideActuelle = 0;
+let slide = document.querySelector('.swiper-wrapper');
+let boutton = document.querySelectorAll('.pagination-button');
 
-function goToSlide(slideIndex) {
-    const slides = document.querySelector('.swiper-wrapper');
-    const buttons = document.querySelectorAll('.pagination-button');
+function changerSlide(indexSlide) {
+    const slide = document.querySelector('.swiper-wrapper');
+    const boutton = document.querySelectorAll('.pagination-button');
     
-    slides.style.transform = `translateX(-${slideIndex * 100}%)`;
-    
-    buttons[currentSlide].classList.remove('active');
-    buttons[slideIndex].classList.add('active');
-    
-    currentSlide = slideIndex;
+    slide.style.transform = `translateX(-${indexSlide * 100}%)`;
+
+    boutton[slideActuelle].classList.remove('active');
+    boutton[indexSlide].classList.add('active');
+
+    slideActuelle = indexSlide;
 }
 
-//Navbar fonctions
-console.log("test");
+// ----------------------------- NAVBAR BOUTON HAMBURGER -----------------------------------
+var navList = document.getElementsByClassName("nav__list")[0];
 function boutonHamburger() {
-    var list = document.getElementsByClassName("nav__list")[0];
     if (window.matchMedia("(max-width:1400px)").matches) {
-        list.classList.toggle('list-active');
+        navList.classList.toggle('list-active');
     } else {
-        list.classList.remove('list-active');
+        navList.classList.remove('list-active');
     }
 }
 
 function TailleFenetre() {
-    var list = document.getElementsByClassName("nav__list")[0];
     if (window.innerWidth >= 1400) {
-        list.classList.remove('list-active');
+        navList.classList.remove('list-active');
     } else {
-        list.classList.remove('list-active');
+        navList.classList.remove('list-active');
     }
 }
+
+window.addEventListener('resize', TailleFenetre);
 
 gsap.set(".imgerrerno", { opacity: 1, rotation: 0, x: 0, y: 0 });
 
 // Create a timeline for the fade transition with more fun effects
-gsap.timeline({ repeat: -1, yoyo: true })
-    .to(".imgerrerno", {
-        duration: 2,
-        opacity: 0,
-        rotation: -90,     
-        ease: "bounce.out",
-    })
-    .to(".imgerrerno", {
-        duration: 2,
-        opacity: 1,
-        rotation: 0,       // Reset rotation
-        ease: "bounce.out"
-    });
+gsap
+  .timeline({ repeat: -1, yoyo: true })
+  .to(".imgerrerno", {
+    duration: 2,
+    opacity: 0,
+    rotation: -90,
+    ease: "bounce.out",
+  })
+  .to(".imgerrerno", {
+    duration: 2,
+    opacity: 1,
+    rotation: 0, // Reset rotation
+    ease: "bounce.out",
+  });
+
+// ----------------------------- GSAP -----------------------------------------
+
+// Bouton hanburger
+
+const hamburger = document.querySelector('.menu-hamburger');
+
+hamburger.addEventListener('mouseover', () => {
+  gsap.to(hamburger, {
+    scale: 1.2,
+    duration: 0.2
+    }
+  );
+});
+
+hamburger.addEventListener('mouseout', () => {
+    gsap.to(hamburger, {
+        scale: 1,
+        duration: 0.2
+    }
+    );
+  });
+
+// Don page d'accueil
+const cinqDollar = document.querySelector(".wrap-5-dollar");
+const vingtDollar = document.querySelector(".wrap-20-dollar");
+const cinquanteDollar = document.querySelector(".wrap-50-dollar");
+const boutonDon = document.querySelector(".wrap-faire-don");
+const sectionDon = document.querySelector(".soutenir__block");
+
+sectionDon.addEventListener("mouseover", function () {
+  gsap
+    .timeline()
+    .to(cinqDollar, { scale: 1.2, duration: 0.2 })
+    .to(vingtDollar, { scale: 1.2, duration: 0.2 })
+    .to(cinquanteDollar, { scale: 1.2, duration: 0.2 });
+});
+
+sectionDon.addEventListener("mouseout", function () {
+  gsap
+    .timeline()
+    .to(cinqDollar, { scale: 1, duration: 0.2 })
+    .to(vingtDollar, { scale: 1, duration: 0.2 })
+    .to(cinquanteDollar, { scale: 1, duration: 0.2 });
+});
+
+
