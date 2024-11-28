fd1aa05eed58d51f494b48a94edd0c0ef4d78e67
audreydandurand <112189528+audreydandurand@users.noreply.github.com>
2024-11-28 14:16:07 -0500

    avancement projet

--- wp-content/themes/theme-flhlmq/news-hub.php
+++ wp-content/themes/theme-flhlmq/news-hub.php
@@ -16,23 +16,27 @@ if ( have_posts() ) : // Est-ce que nous avons des pages à afficher ?
 			<section class="news-hub_une">
             <h1 class="news-hub_une__titre"><?php the_title();?></h1>
             <hr class="hr-titre">
+			<?php 
+            $newsOne = get_field( 'news-one');
+            if ( $newsOne ):
+            ?>
             <div class="container-informations">
-                <div class="container-texte">
-                    <h3 class="news-hub_une__texte">
-                        Pétition pour des HLM de qualité
-                    </h3>
-                    <p class="news-hub_une__texte"> La FLHLMQ souhaite récolter 10 000 signatures.</p>
-                    <p class="news-hub_une__texte">11 septembre, 2024</p>
+				<div class="container-texte">
+            
+            <h3  class="news-hub_une__texte"><?php echo esc_html( $newsOne['news-one-title'] ); ?></h3>
+            <p><?php echo esc_html( $newsOne['similar-card-1-content'] ); ?></p>
+                    <p class="news-hub_une__texte"><?php echo esc_html( $newsOne['news-one-summary'] ); ?></p>
+                    <p class="news-hub_une__texte"><?php echo esc_html( $newsOne['news-one-date'] ); ?></p>
                     <a href="./news_article.html">
                         <button class="bouton-lire-article"><?php the_field('news-cta-read'); ?></button>
                     </a>
                     <a href="./news_article.html">
-                        <button class="bouton-savoir-plus">En savoir plus</button>
+                        <button class="bouton-savoir-plus"><?php the_field('news-cta-know-more'); ?></button>
                     </a>
                 </div>
-                <img class="news-hub_une__photo1" src="./sources/medias/actualite_11_sept_petition.png">
+				<img class="news-hub_une__photo1" src="<?php echo esc_url( $newsOne['news-one-img']['url'] ); ?>" alt="<?php echo esc_attr( $newsOne['news-one-img'] ); ?>" />
             </div>
-
+            <?php endif; ?>
         </section>
 
         <hr class="division-sections">
@@ -61,74 +65,106 @@ if ( have_posts() ) : // Est-ce que nous avons des pages à afficher ?
             <div class="swiper-wrapper">
                 <div class="swiper-slide slide-1">
                     <div class="cartes">
+						<?php 
+                        $newsContinu1 = get_field( 'news-continu-1');
+                        if ( $newsContinu1 ):
+                        ?>
                         <div class="carte--01 carte">
-                            <img src="./sources/medias/actualite_11_sept_mepris.png">
-                            <div class="carte__textes">
-                                <h3>Le mépris a assez duré !</h3>
-                                <p class="carte__textes__sous-titre">L'Office d'habitation du lac Abitibi doit
-                                    changer
-                                    d'attitude.</p>
-                                <p class="carte__textes__date">11 septembre 2024</p>
+                        	<img src="<?php echo esc_url( $newsContinu1['news-continu-img']['url'] ); ?>" alt="<?php echo esc_attr( $newsContinu1['news-continu-img'] ); ?>" />
+							<div class="carte__textes">
+                                <h3><?php echo esc_html( $newsContinu1['news-continu-title'] ); ?></h3>
+                                <p class="carte__textes__sous-titre"><?php echo esc_html( $newsContinu1['news-continu-summary'] ); ?></p>
+                                <p class="carte__textes__date"><?php echo esc_html( $newsContinu1['news-continu-date'] ); ?></p>
                             </div>
-                            <button class="bouton-carte-01 bouton-lire-article"><?php the_field('news-cta-read'); ?></button>
+							<button class="bouton-carte-01 bouton-lire-article"><?php the_field('news-cta-read'); ?></button>
                         </div>
+                        <?php endif; ?>
+
                         <hr class="hr-cartes">
+
+                        <?php 
+                        $newsContinu2 = get_field( 'news-continu-2');
+                        if ( $newsContinu2 ):
+                        ?>
                         <div class="carte--02 carte">
-                            <img src="./sources/medias/actualite_10_sept_unitaines.png">
-                            <div class="carte__textes">
-                                <h3>Première pelletée de terre pour Unitaînés à Rimouski</h3>
-                                <p class="carte__textes__sous-titre">Les offices ne doivent pas dépendre des autres
-                                    pour
-                                    remplir leur mission.</p>
-                                <p class="carte__textes__date">10 septembre, 2024</p>
+						<img src="<?php echo esc_url( $newsContinu2['news-continu-img']['url'] ); ?>" alt="<?php echo esc_attr( $newsContinu2['news-continu-img'] ); ?>" />
+							<div class="carte__textes">
+                                <h3><?php echo esc_html( $newsContinu2['news-continu-title'] ); ?></h3>
+                                <p class="carte__textes__sous-titre"><?php echo esc_html( $newsContinu2['news-continu-summary'] ); ?></p>
+                                <p class="carte__textes__date"><?php echo esc_html( $newsContinu2['news-continu-date'] ); ?></p>
                             </div>
-                            <button class="bouton-carte-02  bouton-lire-article"><?php the_field('news-cta-read'); ?></button>
+							<button class="bouton-carte-02 bouton-lire-article"><?php the_field('news-cta-read'); ?></button>
                         </div>
+                        <?php endif; ?>
+
                         <hr class="hr-cartes">
+
+                        <?php 
+                        $newsContinu3 = get_field( 'news-continu-3');
+                        if ( $newsContinu3 ):
+                        ?>
                         <div class="carte--03 carte">
-                            <img src="./sources/medias/actualite_5_sept_securite.jpg">
-                            <div class="carte__textes">
-                                <h3>Sécurité communautaire dans un HLM de Montréal</h3>
-                                <p class="carte__textes__sous-titre">Les locataires ne sont pas seuls !</p>
-                                <p class="carte__textes__date">5 septembre, 2024</p>
+                        	<img src="<?php echo esc_url( $newsContinu3['news-continu-img']['url'] ); ?>" alt="<?php echo esc_attr( $newsContinu3['news-continu-img'] ); ?>" />
+							<div class="carte__textes">
+                                <h3><?php echo esc_html( $newsContinu3['news-continu-title'] ); ?></h3>
+                                <p class="carte__textes__sous-titre"><?php echo esc_html( $newsContinu3['news-continu-summary'] ); ?></p>
+                                <p class="carte__textes__date"><?php echo esc_html( $newsContinu3['news-continu-date'] ); ?></p>
                             </div>
-                            <button class="bouton-carte-03  bouton-lire-article"><?php the_field('news-cta-read'); ?></button>
+							<button class="bouton-carte-03 bouton-lire-article"><?php the_field('news-cta-read'); ?></button>
                         </div>
+                        <?php endif; ?>
+
                         <hr class="hr-cartes">
-                        <div class="carte--04 carte">
-                            <img src="./sources/medias/actualite_4_sept_manifestation.png">
-                            <div class="carte__textes">
-                                <h3>Manifestation du 15 septembre à Québec</h3>
-                                <p class="carte__textes__sous-titre">On marche derrière la bannière « Je veux un HLM
-                                    » !
-                                </p>
-                                <p class="carte__textes__date">4 septembre, 2024</p>
-                            </div>
-                            <button class="bouton-carte-04  bouton-lire-article"><?php the_field('news-cta-read'); ?></button>
-                        </div>
+                        
+						<?php 
+                        $newsContinu4 = get_field( 'news-continu-4');
+						if ( $newsContinu4 ) :
+						?>
+						<div class="carte--04 carte">
+							<img src="<?php echo esc_url( $newsContinu4['news-continu-img']['url'] ); ?>" alt="<?php echo esc_attr( $newsContinu4['news-continu-img'] ); ?>" />
+							<div class="carte__textes">
+								<h3><?php echo esc_html( $newsContinu4['news-continu-title'] ); ?></h3>
+								<p class="carte__textes__sous-titre"><?php echo esc_html( $newsContinu4['news-continu-summary'] ); ?></p>
+								<p class="carte__textes__date"><?php echo esc_html( $newsContinu4['news-continu-date'] ); ?></p>
+							</div>
+							<button class="bouton-carte-04 bouton-lire-article"><?php the_field('news-cta-read'); ?></button>
+						</div>
+						<?php endif; ?>
+						
+
                         <hr class="hr-cartes">
+
+                        <?php 
+                        $newsContinu5 = get_field( 'news-continu-5');
+                        if ( $newsContinu5 ):
+                        ?>
                         <div class="carte--05 carte">
-                            <img src="./sources/medias/actualite_3_sept_demande.png">
-                            <div class="carte__textes">
-                                <h3>Demande record de HLM à Saguenay</h3>
-                                <p class="carte__textes__sous-titre">La liste explose de 200-300 à 454 personnes en
-                                    attente.</p>
-                                <p class="carte__textes__date">3 septembre, 2024</p>
+                        	<img src="<?php echo esc_url( $newsContinu5['news-continu-img']['url'] ); ?>" alt="<?php echo esc_attr( $newsContinu5['news-continu-img'] ); ?>" />
+							<div class="carte__textes">
+                                <h3><?php echo esc_html( $newsContinu5['news-continu-title'] ); ?></h3>
+                                <p class="carte__textes__sous-titre"><?php echo esc_html( $newsContinu5['news-continu-summary'] ); ?></p>
+                                <p class="carte__textes__date"><?php echo esc_html( $newsContinu5['news-continu-date'] ); ?></p>
                             </div>
-                            <button class="bouton-carte-05  bouton-lire-article"><?php the_field('news-cta-read'); ?></button>
+							<button class="bouton-carte-05 bouton-lire-article"><?php the_field('news-cta-read'); ?></button>
                         </div>
+                        <?php endif; ?>
+
                         <hr class="hr-cartes">
+
+                        <?php 
+                        $newsContinu6 = get_field( 'news-continu-6');
+                        if ( $newsContinu6 ):
+                        ?>
                         <div class="carte--06 carte">
-                            <img src="./sources/medias/actualite_30_aout_voie.png">
-                            <div class="carte__textes">
-                                <h3>Une nouvelle voie rapide pour les développeurs</h3>
-                                <p class="carte__textes__sous-titre">Les offices d'habitation doivent être du
-                                    nombre.
-                                </p>
-                                <p class="carte__textes__date">30 août, 2024</p>
+                        	<img src="<?php echo esc_url( $newsContinu6['news-continu-img']['url'] ); ?>" alt="<?php echo esc_attr( $newsContinu6['news-continu-img'] ); ?>" />
+							<div class="carte__textes">
+                                <h3><?php echo esc_html( $newsContinu6['news-continu-title'] ); ?></h3>
+                                <p class="carte__textes__sous-titre"><?php echo esc_html( $newsContinu6['news-continu-summary'] ); ?></p>
+                                <p class="carte__textes__date"><?php echo esc_html( $newsContinu6['news-continu-date'] ); ?></p>
                             </div>
-                            <button class="bouton-carte-06  bouton-lire-article"><?php the_field('news-cta-read'); ?></button>
+							<button class="bouton-carte-06 bouton-lire-article"><?php the_field('news-cta-read'); ?></button>
                         </div>
+                        <?php endif; ?>
                     </div>
                 </div>
                 <div class="swiper-slide slide-2">
