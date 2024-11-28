65f42f977e1d716927b0869ef7b27ffc2a0b1520
audreydandurand <112189528+audreydandurand@users.noreply.github.com>
2024-11-28 11:58:29 -0500

    similar cards

--- wp-content/themes/theme-flhlmq/news-article.php
+++ wp-content/themes/theme-flhlmq/news-article.php
@@ -28,14 +28,19 @@ if ( have_posts() ) : // Est-ce que nous avons des pages à afficher ?
             <div class="div-prochaine-nouvelle"><h3 class="prochaine-nouvelle"><?php the_field('next-news-cta'); ?></h3></div>
             </div>
         </section>
+        
 		<section class="news-article__similaire">
             <h1 class="news-article__similaire__titre"><?php the_field('similar-title'); ?></h1>
             <hr class="hr-titre">
             <div class="cartes-swiper">
                 <div class="swiper-container2 mySwiper ">
                     <div class="swiper-wrapper">
+
                         <div class="swiper-slide slide-1">
-                        <?php if ( $similarCard1 ): ?>
+                        <?php 
+                        $similarCard1 = get_field( 'similar-card-1');
+                        if ( $similarCard1 ):
+                        ?>
                         <div class="carte--01 carte">
                         <img src="<?php echo esc_url( $similarCard1['similar-card-1-img']['url'] ); ?>" alt="<?php echo esc_attr( $similarCard1['similar-card-1-title'] ); ?>" />
                         <h3><?php echo esc_html( $similarCard1['similar-card-1-title'] ); ?></h3>
@@ -44,28 +49,65 @@ if ( have_posts() ) : // Est-ce que nous avons des pages à afficher ?
                         <?php endif; ?>
                         </div>
                         <div class="swiper-slide slide-2">
-                            <div class="carte--02 carte">
-							<?php the_field('similar-card-2'); ?>
-                            </div>
+                        <?php 
+                        $similarCard2 = get_field( 'similar-card-2');
+                        if ( $similarCard2 ):
+                        ?>
+                        <div class="carte--02 carte">
+                        <img src="<?php echo esc_url( $similarCard2['similar-card-2-img']['url'] ); ?>" alt="<?php echo esc_attr( $similarCard2['similar-card-2-title'] ); ?>" />
+                        <h3><?php echo esc_html( $similarCard2['similar-card-2-title'] ); ?></h3>
+                        <p><?php echo esc_html( $similarCard2['similar-card-2-content'] ); ?></p>
+                        </div>
+                        <?php endif; ?>
                         </div>
                         <div class="swiper-slide slide-3">
-                            <div class="carte--03 carte">
-							<?php the_field('similar-card-3'); ?>
-                            </div>
+                        <?php 
+                        $similarCard3 = get_field( 'similar-card-3');
+                        if ( $similarCard3 ):
+                        ?>
+                        <div class="carte--03 carte">
+                        <img src="<?php echo esc_url( $similarCard3['similar-card-3-img']['url'] ); ?>" alt="<?php echo esc_attr( $similarCard3['similar-card-3-title'] ); ?>" />
+                        <h3><?php echo esc_html( $similarCard3['similar-card-3-title'] ); ?></h3>
+                        <p><?php echo esc_html( $similarCard3['similar-card-3-content'] ); ?></p>
+                        </div>
+                        <?php endif; ?>
                         </div>
                     </div>
                 </div>
             </div>
             <div class="cartes">
-                <div class="carte--01 carte">
-				<?php the_field('similar-card-1-img'); ?>
-                </div>
-                <div class="carte--02 carte">
-				<?php the_field('similar-card-2'); ?>
-                </div>
-                <div class="carte--03 carte">
-				<?php the_field('similar-card-3'); ?>
-                </div>
+                        <?php 
+                        $similarCard1 = get_field( 'similar-card-1');
+                        if ( $similarCard1 ):
+                        ?>
+                        <div class="carte--01 carte">
+                        <img src="<?php echo esc_url( $similarCard1['similar-card-1-img']['url'] ); ?>" alt="<?php echo esc_attr( $similarCard1['similar-card-1-title'] ); ?>" />
+                        <h3><?php echo esc_html( $similarCard1['similar-card-1-title'] ); ?></h3>
+                        <p><?php echo esc_html( $similarCard1['similar-card-1-content'] ); ?></p>
+                        </div>
+                        <?php endif; ?>
+
+                        <?php 
+                        $similarCard2 = get_field( 'similar-card-2');
+                        if ( $similarCard2 ):
+                        ?>
+                        <div class="carte--02 carte">
+                        <img src="<?php echo esc_url( $similarCard2['similar-card-2-img']['url'] ); ?>" alt="<?php echo esc_attr( $similarCard2['similar-card-2-title'] ); ?>" />
+                        <h3><?php echo esc_html( $similarCard2['similar-card-2-title'] ); ?></h3>
+                        <p><?php echo esc_html( $similarCard2['similar-card-2-content'] ); ?></p>
+                        </div>
+                        <?php endif; ?>
+
+                        <?php 
+                        $similarCard3 = get_field( 'similar-card-3');
+                        if ( $similarCard3 ):
+                        ?>
+                        <div class="carte--03 carte">
+                        <img src="<?php echo esc_url( $similarCard3['similar-card-3-img']['url'] ); ?>" alt="<?php echo esc_attr( $similarCard3['similar-card-3-title'] ); ?>" />
+                        <h3><?php echo esc_html( $similarCard3['similar-card-3-title'] ); ?></h3>
+                        <p><?php echo esc_html( $similarCard3['similar-card-3-content'] ); ?></p>
+                        </div>
+                        <?php endif; ?>
             </div>
         </section>
 
