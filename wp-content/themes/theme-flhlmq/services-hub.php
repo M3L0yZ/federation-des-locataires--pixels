5137b8d75826b1cda01f844be7ae39dcf182f5e5
Cam <112569387+cam456@users.noreply.github.com>
2024-12-05 11:04:24 -0500

    merge_servicehub

--- wp-content/themes/theme-flhlmq/services-hub.php
+++ wp-content/themes/theme-flhlmq/services-hub.php
@@ -11,172 +11,173 @@ if ( have_posts() ) : // Est-ce que nous avons des pages à afficher ?
 	while ( have_posts() ) : the_post(); 
 ?>
 
-	<article>
-		<?php if (!is_front_page()) : // Si nous ne sommes PAS sur la page d'accueil ?>
-			<section class="service">
-            <h1 class="service__titreservice">Nos services</h1>
-            <hr class="hr-titre">
-		
-            			<div class="service__congrebackgroundcontainer">
-                			<div class="service__congrecontainer">
+<article>
+    <?php if (!is_front_page()) : // Si nous ne sommes PAS sur la page d'accueil ?>
+    <section class="service">
+        <h1 class="service__titreservice">Nos services</h1>
+        <hr class="hr-titre">
 
-				
-								<?php
-								$card1 = get_field('card-1');
-									if( $card1 ): ?>
-									<div class="service__insidecontainer__congres">
-									
-									<div  class="service__shadowcolor">
+        <div class="service__congrebackgroundcontainer">
+            <div class="service__congrecontainer">
 
-										
-										<div class="service__titrecongre">
-											<h2 class="service__congretitre"> <?php echo $card1['card-1-title']; ?> </h2>
-										</div>
 
-										<div class="service__textcongres">
-											<p class="service__congretexte"> 
-												<?php echo $card1['card-1-summary']; ?>
-											</p>
-										</div>
+                <?php
+				$card1 = get_field('card-1');
+				if( $card1 ): ?>
+                <div class="service__insidecontainer__congres">
 
-										<div class="service__ensavoirplus">
-											<a href="./service.html"><button class="bouttonsavoirplus"><?php echo $card1['card-1-cta']; ?></button></a>
-										</div>
+                    <div class="service__shadowcolor">
 
-									</div>
-									</div>
-								<?php endif; ?>
-								
+
+                        <div class="service__titrecongre">
+                            <h2 class="service__congretitre"> <?php echo $card1['card-1-title']; ?> </h2>
+                        </div>
+
+                        <div class="service__textcongres">
+                            <p class="service__congretexte">
+                                <?php echo $card1['card-1-summary']; ?>
+                            </p>
+                        </div>
+
+                        <div class="service__ensavoirplus">
+                            <a href="./service.html"><button
+                                    class="bouttonsavoirplus"><?php echo $card1['card-1-cta']; ?></button></a>
+                        </div>
+
+                    </div>
                 </div>
+                <?php endif; ?>
+
             </div>
+        </div>
 
-            <br>
-            <br>
-            
+        <br>
+        <br>
 
-            <div class="service__associationbackgroundcontainer">
-                <div class="service__congrecontainer">
-                    
-                    <div class="service__insidecontainer__association">
 
-								<?php
+        <div class="service__associationbackgroundcontainer">
+            <div class="service__congrecontainer">
+
+                <div class="service__insidecontainer__association">
+
+                    <?php
 									$card2 = get_field('card-2');
 										if( $card2 ): ?>
-										<div class="service__insidecontainer__congres">
-										
-											<div  class="service__shadowcolor">
+                    <div class="service__insidecontainer__congres">
 
-												
-												<div class="service__titrecongre">
-													<h2 class="service__congretitre"> <?php echo $card2['card-2-title']; ?> </h2>
-												</div>
+                        <div class="service__shadowcolor">
 
-												<div class="service__textcongres">
-													<p class="service__congretexte"> 
-														<?php echo $card2['card-2-summary']; ?>
-													</p>
-												</div>
 
-												<div class="service__ensavoirplus">
-													<button class="bouttonsavoirplus"><?php echo $card2['card-2-cta']; ?></button>
-												</div>
+                            <div class="service__titrecongre">
+                                <h2 class="service__congretitre"> <?php echo $card2['card-2-title']; ?> </h2>
+                            </div>
 
-											</div>
+                            <div class="service__textcongres">
+                                <p class="service__congretexte">
+                                    <?php echo $card2['card-2-summary']; ?>
+                                </p>
+                            </div>
 
-										</div>
-								<?php endif; ?>
-                        
+                            <div class="service__ensavoirplus">
+                                <button class="bouttonsavoirplus"><?php echo $card2['card-2-cta']; ?></button>
+                            </div>
+
+                        </div>
+
                     </div>
+                    <?php endif; ?>
+
                 </div>
             </div>
+        </div>
 
-            <br>
-            <br>
+        <br>
+        <br>
 
-            <div class="service__ccrbackgroundcontainer">
-                <div class="service__congrecontainer">
-                    
-                    <div class="service__insidecontainer__ccr">
+        <div class="service__ccrbackgroundcontainer">
+            <div class="service__congrecontainer">
 
-								<?php
+                <div class="service__insidecontainer__ccr">
+
+                    <?php
 									$card3 = get_field('card-3');
 										if( $card3 ): ?>
-										<div class="service__insidecontainer__congres">
-										
-											<div  class="service__shadowcolor">
+                    <div class="service__insidecontainer__congres">
 
-												
-												<div class="service__titrecongre">
-													<h2 class="service__congretitre"> <?php echo $card3['card-3-title']; ?> </h2>
-												</div>
+                        <div class="service__shadowcolor">
 
-												<div class="service__textcongres">
-													<p class="service__congretexte"> 
-														<?php echo $card3['card-3-summary']; ?>
-													</p>
-												</div>
 
-												<div class="service__ensavoirplus">
-													<button class="bouttonsavoirplus"><?php echo $card3['card-3-cta']; ?></button>
-												</div>
+                            <div class="service__titrecongre">
+                                <h2 class="service__congretitre"> <?php echo $card3['card-3-title']; ?> </h2>
+                            </div>
 
-											</div>
+                            <div class="service__textcongres">
+                                <p class="service__congretexte">
+                                    <?php echo $card3['card-3-summary']; ?>
+                                </p>
+                            </div>
 
-										</div>
-								<?php endif; ?>
-                        
+                            <div class="service__ensavoirplus">
+                                <button class="bouttonsavoirplus"><?php echo $card3['card-3-cta']; ?></button>
+                            </div>
+
+                        </div>
+
                     </div>
+                    <?php endif; ?>
+
                 </div>
             </div>
+        </div>
 
-            <br>
-            <br>
+        <br>
+        <br>
 
-            <div class="service__locatairebackgroundcontainer">
-                <div class="service__congrecontainer">
-                    
-                    <div class="service__insidecontainer__locataire">
-								<?php
+        <div class="service__locatairebackgroundcontainer">
+            <div class="service__congrecontainer">
+
+                <div class="service__insidecontainer__locataire">
+                    <?php
 									$card4 = get_field('card-4');
 										if( $card4 ): ?>
-										<div class="service__insidecontainer__congres">
-										
-											<div  class="service__shadowcolor">
+                    <div class="service__insidecontainer__congres">
 
-												
-												<div class="service__titrecongre">
-													<h2 class="service__congretitre"> <?php echo $card3['card-4-title']; ?> </h2>
-												</div>
+                        <div class="service__shadowcolor">
 
-												<div class="service__textcongres">
-													<p class="service__congretexte"> 
-														<?php echo $card4['card-4-summary']; ?>
-													</p>
-												</div>
 
-												<div class="service__ensavoirplus">
-													<button class="bouttonsavoirplus"><?php echo $card4['card-4-cta']; ?></button>
-												</div>
+                            <div class="service__titrecongre">
+                                <h2 class="service__congretitre"> <?php echo $card3['card-4-title']; ?> </h2>
+                            </div>
 
-											</div>
+                            <div class="service__textcongres">
+                                <p class="service__congretexte">
+                                    <?php echo $card4['card-4-summary']; ?>
+                                </p>
+                            </div>
 
-										</div>
-								<?php endif; ?>
-                        
+                            <div class="service__ensavoirplus">
+                                <button class="bouttonsavoirplus"><?php echo $card4['card-4-cta']; ?></button>
+                            </div>
+
+                        </div>
+
                     </div>
+                    <?php endif; ?>
+
                 </div>
             </div>
+        </div>
 
-            <br>
-            <br>
-               
+        <br>
+        <br>
 
-       
 
-        </section>
-		<?php endif; ?>
-		
-	</article>
+
+
+    </section>
+    <?php endif; ?>
+
+</article>
 <?php endwhile; // Fermeture de la boucle
 
 else : // Si aucune page n'a été trouvée
