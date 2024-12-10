6093cd031fafc3c797452fb98de005a1b34ab9ae
audreydandurand <112189528+audreydandurand@users.noreply.github.com>
2024-12-09 20:23:30 -0500

    nav

--- wp-content/themes/theme-flhlmq/header.php
+++ wp-content/themes/theme-flhlmq/header.php
@@ -93,12 +93,14 @@
 						echo '<li class="nav__item"><a href="' . esc_url($menu_items[2]->url) . '">' . esc_html($menu_items[2]->title) . '</a></li>';
 					}
 					if ($menu_items[3]) {
-						echo '<li class="nav__item"><a href="' . esc_url($menu_items[3]->url) . '">' . esc_html($menu_items[3]->title) . '</a></li>';
+						echo '<li class="nav__item"><img class="icone-facebook" src="' . esc_url($menu_items[3]->url) . '"></li>';
 					}
 					if ($menu_items[4]) {
-						echo '<li class="nav__item"><a href="' . esc_url($menu_items[4]->url) . '">' . esc_html($menu_items[4]->title) . '</a></li>';
+						echo '<li class="nav__item"><img class="icone-youtube" src="' . esc_url($menu_items[4]->url) . '"></li>';
 					}
 				?>
+				<li class="nav__item"><img class="icone-facebook" src="./sources/icons/facebook.png"></li>
+				<li class="nav__item"><img class="icone-youtube" src="./sources/icons/youtube.png"></li>
 				<li class="nav__item">
 					<input type="search" id="search" name="search" placeholder="Rechercher">
 				</li>
