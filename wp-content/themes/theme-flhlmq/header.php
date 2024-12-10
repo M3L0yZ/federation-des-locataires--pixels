f7620a9fd66d6d8accffbd9e8a7a7168deb4f581
audreydandurand <112189528+audreydandurand@users.noreply.github.com>
2024-12-09 20:13:45 -0500

    navigation

--- wp-content/themes/theme-flhlmq/header.php
+++ wp-content/themes/theme-flhlmq/header.php
@@ -69,12 +69,40 @@
             </div>
         </div>
 <?php endif; ?>
-		<nav>
-			<?php 
-				// Affiche un menu si dans le tableau de bord un menu a été défini dans cet emplacement
-				wp_nav_menu( array( 'theme_location' => 'main-menu' ) );
-			?>
+		<nav class="nav">
+			<img class="logo-search" src="./sources/icons/symbole-de-linterface-de-recherche_light.png">
+			<img class="logo-langue" src="./sources/icons/world.png">
+			<a href="javascript:void(0);" class="menu-hamburger" onclick="boutonHamburger()">
+				<i class="fa fa-bars"></i>
+			</a>
+			<ul class="nav__list">
+				<?php
+					//Récupération de l'emplacement du menu de navigation
+					$menu_id = get_nav_menu_locations()['main-menu'];
+
+					//Récupération de tous les éléments du menu de navigation
+					$menu_items = wp_get_nav_menu_items($menu_id);
+
+					if ($menu_items[0]) {
+						echo '<li class="nav__item"><a href="' . esc_url($menu_items[0]->url) . '">' . esc_html($menu_items[0]->title) . '</a></li>';
+					}
+					if ($menu_items[1]) {
+						echo '<li class="nav__item"><a href="' . esc_url($menu_items[1]->url) . '">' . esc_html($menu_items[1]->title) . '</a></li>';
+					}
+					if ($menu_items[2]) {
+						echo '<li class="nav__item"><a href="' . esc_url($menu_items[2]->url) . '">' . esc_html($menu_items[2]->title) . '</a></li>';
+					}
+				?>
+				<li class="nav__item"><img class="icone-facebook" src="./sources/icons/facebook.png"></li>
+				<li class="nav__item"><img class="icone-youtube" src="./sources/icons/youtube.png"></li>
+				<li class="nav__item">
+					<input type="search" id="search" name="search" placeholder="Rechercher">
+				</li>
+				<li class="nav__item"><a href="#">FR/EN</a></li>
+			</ul>
 		</nav>
+		
+        
 
 		<?php 
 			// Affiche la description de site se trouvant dans "General Settings" dans l'admin WordPress
